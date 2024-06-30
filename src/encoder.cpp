1b890e812810cee90af2b312e03f2d5f537a0df0
HenryKim12 <henrykim966@gmail.com>
2024-06-30 16:58:46 -0700

    update text_to_binary tests

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -5,12 +5,32 @@
 #include <bitset>
 #include <vector>
 #include <stdexcept>
+#include "./util/RGB.hpp"
 
 using namespace std;
 
 Encoder::Encoder() {}
 
 cv::Mat Encoder::encode(cv::Mat image, string message) {
+    vector<int> binary_message = text_to_binary(message);
+    Image image_helper;
+    vector<vector<RGB>> img_rgbs = image_helper.pixels_to_rgb(image);
+
+    int msg_bit_length = binary_message.size() * 8;
+    int img_bit_length = img_rgbs.size() * img_rgbs[0].size() * 3;
+    if (msg_bit_length > img_bit_length) {
+        cout << "Error (Encoder::encode): message is too large to encode into image" << endl;
+        throw invalid_argument("Error (Encoder::encode): message is too large to encode into image");
+    }
+
+    vector<int> msg_bit_list(msg_bit_length);
+    for (int i = 0; i < binary_message.size(); i++) {
+        for (int j = 0; j < 8; j++) {
+            int bit = (binary_message[i] >> j) & 1;
+            msg_bit_list.push_back(bit);
+        }
+    }
+
     // vector<bitset<8>> binary_message = text_to_binary(message);
     // Image image_helper;
     // vector<vector<bitset<8>>> img_pixels = image_helper.pixels_to_binary(image);
--- tests/test-encoder.cpp
+++ tests/test-encoder.cpp
@@ -9,23 +9,19 @@
 TEST_CASE("Text to Binary: char", "[encoder]") {
     Encoder encoder;
     std::vector<int> binaries = encoder.text_to_binary("A");
-
-    // std::bitset<8> A_bits(65);
-    // std::vector<std::bitset<8>> answer = {A_bits};
-
-    // std::cout << "answer " << answer[0] << " binaries " << binaries[0] << std::endl;
-    // REQUIRE(binaries[0] == answer[0]);
+    int A_ascii = int('A');
+    std::vector<int> answer = { A_ascii };
+    REQUIRE(binaries[0] == answer[0]);
 }
 
 TEST_CASE("Text to Binary: string", "[encoder]") {
     Encoder encoder;
     std::vector<int> binaries = encoder.text_to_binary("ABC");
-
-    // std::bitset<8> A_bits(65);
-    // std::bitset<8> B_bits(66);
-    // std::bitset<8> C_bits(67);
-    // std::vector<std::bitset<8>> answer = {A_bits, B_bits, C_bits};
-    // for (int i = 0; i < binaries.size(); i++) {
-    //     REQUIRE(binaries[i] == answer[i]);
-    // }
+    int A_ascii = int('A');
+    int B_ascii = int('B');
+    int C_ascii = int('C');
+    std::vector<int> answer = { A_ascii, B_ascii, C_ascii };
+    for (int i = 0; i < binaries.size(); i++) {
+        REQUIRE(binaries[i] == answer[i]);
+    }
 }
